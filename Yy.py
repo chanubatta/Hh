b5456d82517a45bbd82c56481cc969e0951f3fc7
chanubatta <72717444+chanubatta@users.noreply.github.com>
2023-11-14 07:31:58 +0530

    Yy.py

---
+++ Yy.py
@@ -0,0 +1,34 @@
+#WRITEN BY MR SL CHANUKA
+#MY GITHUB :http://github.com/chanubatta
+#--------import-------#
+from os import system as clr
+#--------logo-------#
+logo=("""
+██████  ██▓        ██░ ██  ▄▄▄       ▄████▄   ██ ▄█▀▓█████  ██▀███  
+▒██    ▒ ▓██▒       ▓██░ ██▒▒████▄    ▒██▀ ▀█   ██▄█▒ ▓█   ▀ ▓██ ▒ ██▒
+░ ▓██▄   ▒██░       ▒██▀▀██░▒██  ▀█▄  ▒▓█    ▄ ▓███▄░ ▒███   ▓██ ░▄█ ▒
+  ▒   ██▒▒██░       ░▓█ ░██ ░██▄▄▄▄██ ▒▓▓▄ ▄██▒▓██ █▄ ▒▓█  ▄ ▒██▀▀█▄  
+▒██████▒▒░██████▒   ░▓█▒░██▓ ▓█   ▓██▒▒ ▓███▀ ░▒██▒ █▄░▒████▒░██▓ ▒██▒
+▒ ▒▓▒ ▒ ░░ ▒░▓  ░    ▒ ░░▒░▒ ▒▒   ▓▒█░░ ░▒ ▒  ░▒ ▒▒ ▓▒░░ ▒░ ░░ ▒▓ ░▒▓░
+░ ░▒  ░ ░░ ░ ▒  ░    ▒ ░▒░ ░  ▒   ▒▒ ░  ░  ▒   ░ ░▒ ▒░ ░ ░  ░  ░▒ ░ ▒░
+░  ░  ░    ░ ░       ░  ░░ ░  ░   ▒   ░        ░ ░░ ░    ░     ░░   ░ 
+      ░      ░  ░    ░  ░  ░      ░  ░░ ░      ░  ░      ░  ░   ░     
+                                      ░                               """)
+#-------clear-------#
+def clear():
+	clr('clear')
+	print(logo)
+	print(45*'-')
+	print('SL Power Gang']
+	print('Facebook :www.facebook.com\Chanuka.Ravindu.12')
+	print('whatsapp number : +94740074336')
+	print('GitHub : SL Power Gang')
+	print(40*'-')
+#--------main------#
+def main():
+	clear()
+	print('[01]Facebook Hack') 
+	print('[02]Exit')
+	option=input('[CHOICE MENU : ')
+#--------end--------#
+clear()
